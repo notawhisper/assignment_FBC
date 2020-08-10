ee59b474527c3e5d08b120afb67b3ef6d57cb049
shotaro yamamoto <shotoron@gmail.com>
2020-08-10 14:07:09 +0900

    [add] Userモデルにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
+  before_validation { email.downcase! }
   validates :name,  presence: true, length: { maximum: 30 }
- validates :email, presence: true, length: { maximum: 255 },
+  validates :email, presence: true, length: { maximum: 255 }, uniqueness: true,
                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
