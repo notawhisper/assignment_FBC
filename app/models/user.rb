317b258719aacfea511e787c08de881f5d6af41b
shotaro yamamoto <shotoron@gmail.com>
2020-08-16 19:39:23 +0900

    [add] ログイン/ログアウト機能を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 }, uniqueness: true,
                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
                    has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }
+  # has_many :pictures
 end
