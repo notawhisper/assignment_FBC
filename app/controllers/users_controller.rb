7459d958bcfcec9bf473d6d8db1938c9fd030a05
shotaro yamamoto <shotoron@gmail.com>
2020-08-18 16:49:41 +0900

    [Remove]不要なviewファイルを削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,11 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
-
-  def index
-    @users = User.all
-  end
+  before_action :set_user, only: [:show, :destroy]
 
   def show
   end
 
   def new
     @user = User.new
-  end
-
-  def edit
   end
 
   def create
@@ -24,18 +17,6 @@ class UsersController < ApplicationController
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
