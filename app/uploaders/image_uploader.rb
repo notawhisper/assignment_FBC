dede11dd1c129dddd57716c10de6ccb38eff284b
shotaro yamamoto <shotoron@gmail.com>
2020-08-19 18:57:00 +0900

    [fix]pictureを投稿できない不具合を解決、画像投稿時のリサイズを設定

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,11 +1,9 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   storage :file
-  process resize_to_limit: [300, 200, "center"]
+  process :resize_and_pad => [500, 500, "center"]
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-
-
 end
