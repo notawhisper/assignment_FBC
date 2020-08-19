ea502acdf109d9523838edf6be45d45406158dac
shotaro yamamoto <shotoron@gmail.com>
2020-08-19 23:04:06 +0900

    [add]404/500エラーページ 、Pictureモデルにバリデーションを追加

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,10 @@
 class Picture < ApplicationRecord
   belongs_to :user
   mount_uploader :image, ImageUploader
+  validates :image_or_text, presence: true
+
+  private
+  def image_or_text
+    image.presence or text.presence
+  end
 end
